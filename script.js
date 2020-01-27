5056fd2ccd1cd3db5cdb4cd0f1aecb32867f76f6
Tauqueer Khan <tauqueer.khan@gmail.com>
2020-01-26 23:43:56 -0500

    almost done prelim game

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+let score = 0;
 
+let bricks = [];
+for (column = 0; column < brickColumnCount; column++) {
+  bricks[column] = [];
+  for (row = 0; row < brickRowCount; row++) {
+    bricks[column][row] = {
+      x: 0,
+      y: 0,
+      status: 1,
+    };
+  }
+}
 const keyDownHandler = event => {
   if (event.keyCode === 39) {
     rightPressed = true;
@@ -30,9 +49,22 @@ const keyUpHandler = event => {
   }
 };
 
+const mouseMoveHandler = event => {
+  let relativeX = event.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+};
+
 document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
+document.addEventListener('mousemove', mouseMoveHandler);
 
+const drawScore = () => {
+  context.font = '16px sans-serif';
+  context.fillStyle = '#0095DD';
+  context.fillText(`Score: ${score}`, 8, 20);
+};
 const drawBall = () => {
   context.beginPath();
   context.arc(x, y, ballRadius, (Math.PI / 180) * 0, (Math.PI / 180) * 360);
@@ -54,17 +86,62 @@ const drawPaddle = () => {
   context.closePath();
 };
 
+const collisionDetection = () => {
+  for (column = 0; column < brickColumnCount; column++) {
+    for (row = 0; row < brickRowCount; row++) {
+      let brick = bricks[column][row];
+      if (brick.status === 1) {
+        if (
+          x > brick.x &&
+          x < brick.x + brickWidth + ballRadius &&
+          y > brick.y &&
+          y < brick.y + brickHeight + ballRadius
+        ) {
+          dy = -dy;
+          brick.status = 0;
+          score++;
+          if (score === brickColumnCount * brickRowCount) {
+            alert('You Win!');
+            window.location.reload();
+          }
+        }
+      }
+    }
+  }
+};
+
+const drawBricks = () => {
+  for (column = 0; column < brickColumnCount; column++) {
+    for (row = 0; row < brickRowCount; row++) {
+      if (bricks[column][row].status === 1) {
+        let brickX = column * (brickWidth + brickPadding) + brickOffsetLeft;
+        let brickY = row * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[column][row].x = brickX;
+        bricks[column][row].y = brickY;
+        context.beginPath();
+        context.rect(brickX, brickY, brickWidth, brickHeight);
+        context.fillStyle = '#0095DD';
+        context.fill();
+        context.closePath();
+      }
+    }
+  }
+};
 const draw = () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
-
+  drawBricks();
+  collisionDetection();
+  drawScore();
   // check to see if the ball is on the edge, then reverse directions
   if (y + dy < ballRadius) dy = -dy;
   if (y + dy > canvas.height - ballRadius) {
     if (x > paddleX && x < paddleX + paddleWidth) dy = -dy;
     else {
-      alert('GAME OVER');
+      // alert('You Lose!');
+      x = canvas.width / 2;
+      y = canvas.height - 30;
       document.location.reload();
     }
   }
@@ -75,4 +152,4 @@ const draw = () => {
   y += dy;
 };
 
-setInterval(draw, 10);
+setInterval(draw, 20);
